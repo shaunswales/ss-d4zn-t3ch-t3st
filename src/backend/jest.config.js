e7db6b172434cc219cb4a924d3cd994ae14a22ed
Shaun Swales <shaunswales@outlook.com>
2020-07-22 11:58:37 +0100

    JEST config switcheroo

--- src/backend/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  moduleFileExtensions: ['js', 'json'],
-  rootDir: './',
-  testRegex: '.spec.js$',
-  testEnvironment: 'node',
-  setupFiles: ['<rootDir>jest.setup.js'],
-  testPathIgnorePatterns: ['/node_modules/'],
-};
