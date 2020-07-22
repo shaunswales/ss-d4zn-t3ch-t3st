aebcf77aa757ccc87905103e29a00337d1a20ff1
Shaun Swales <shaunswales@outlook.com>
2020-07-22 11:46:36 +0100

    Added empty commands to pad out later

--- src/frontend/src/plugins/index.js
+++ src/frontend/src/plugins/index.js
@@ -1,3 +1,6 @@
 /**
  * @type {Cypress.PluginConfig}
  */
+module.exports = function (on, config) {
+  // configure plugins here
+};
---
+++ src/frontend/src/support/commands.js
@@ -0,0 +1,12 @@
+Cypress.Commands.add('checkTheWeather', (postcode) => {
+  // Search for Postcode
+  // Switch
+  // Weather Details
+  // Get JSON Response  
+  // Assert Results
+});
+
+Cypress.Commands.add(
+  'checkTheWeatherInvalid',
+  (postcode, expectedOutcome) => {},
+);
